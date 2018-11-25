37b53b2852e568160eb03f977b2baaf884f372f9
LaroyenneG <laroyenneg@gmail.com>
2018-11-25 13:08:42 +0100

    organ

---
+++ src/robot/element/terminal/organ/Pliers.java
@@ -0,0 +1,9 @@
+package robot.element.terminal.organ;
+
+public class Pliers extends TerminalOrgan {
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
---
+++ src/robot/element/terminal/organ/TerminalOrgan.java
@@ -0,0 +1,7 @@
+package robot.element.terminal.organ;
+
+import robot.element.Element;
+
+public abstract class TerminalOrgan extends Element {
+
+}
