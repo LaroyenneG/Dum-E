62925d7d46e77477f84bcabd5035ce07ec484168
laroyenneg <laroyenneg@gmail.com>
2018-11-30 15:57:37 +0100

    orbitator

--- src/view/ElementDraftman.java
+++ src/view/ElementDraftman.java
@@ -1,12 +1,14 @@
 package view;
 
 
+import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
 import com.sun.j3d.utils.geometry.Cone;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import model.ElementVisitor;
 import model.element.terminal.organ.Default;
 
 import javax.media.j3d.*;
+import javax.vecmath.Point3d;
 
 public class ElementDraftman implements ElementVisitor {
 
@@ -16,13 +18,19 @@ public class ElementDraftman implements ElementVisitor {
     public ElementDraftman(Canvas3D canvas3D) {
         theScene = new BranchGroup();
         universe = new SimpleUniverse(canvas3D);
-        universe.getViewingPlatform().setNominalViewingTransform();
         universe.cleanup();
+        universe.getViewingPlatform().setNominalViewingTransform();
+        addOrbitator(canvas3D);
     }
 
+    private void addOrbitator(Canvas3D canvas3D) {
+
+        OrbitBehavior ob = new OrbitBehavior(canvas3D);
+        ob.setSchedulingBounds(new BoundingSphere(new Point3d(), Double.MAX_VALUE));
+        universe.getViewingPlatform().setViewPlatformBehavior(ob);
+    }
 
     public void display() {
-
         theScene.compile();
         universe.addBranchGraph(theScene);
     }
