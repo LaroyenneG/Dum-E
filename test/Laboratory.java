4d6eb8636bb82ae0093634e2359c206815c75502
laroyenneg <laroyenneg@gmail.com>
2018-12-02 18:20:05 +0100

    labo

--- test/Laboratory.java
+++ test/Laboratory.java
@@ -1,95 +1,77 @@
 import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
-import com.sun.j3d.utils.geometry.Cylinder;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 import javax.media.j3d.*;
 import javax.swing.*;
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
-import javax.vecmath.Vector3d;
 import java.awt.*;
 
 
-public class Ouf3D extends JFrame {
+public class Laboratory extends JFrame {
 
-    //The canvas to be drawn upon.
-    public Canvas3D myCanvas3D;
+    private Laboratory(Node node) {
 
-
-    public Ouf3D() {
-        //Mechanism for closing the window and ending the program.
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Canvas3D myCanvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
 
-        //Default settings for the viewer parameters.
-        myCanvas3D = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
-
-
-        //Construct the SimpleUniverse:
-        //First generate it using the Canvas.
         SimpleUniverse simpUniv = new SimpleUniverse(myCanvas3D);
 
-
-        //Default position of the viewer.
         simpUniv.getViewingPlatform().setNominalViewingTransform();
 
+        createSceneGraph(simpUniv, node);
 
-        //The scene is generated in this method.
-        createSceneGraph(simpUniv);
-
-
-        //Add some light to the scene.
         addLight(simpUniv);
 
         addAxis(simpUniv);
 
-
-        //The following three lines enable navigation through the scene using the mouse.
         OrbitBehavior ob = new OrbitBehavior(myCanvas3D);
-        ob.setSchedulingBounds(new BoundingSphere(new Point3d(0.0, 0.0, 0.0), Double.MAX_VALUE));
+        ob.setSchedulingBounds(new BoundingSphere(new Point3d(), Double.MAX_VALUE));
         simpUniv.getViewingPlatform().setViewPlatformBehavior(ob);
 
-
-        //Show the canvas/window.
         setTitle(getClass().getSimpleName());
-        setSize(700, 700);
+        setSize(900, 900);
         getContentPane().add("Center", myCanvas3D);
         setVisible(true);
+        setResizable(false);
     }
 
+    /**
+     * =====> User control <======
+     */
+    private static BranchGroup observable() {
+
+        BranchGroup branchGroup = new BranchGroup();
+
+        /*
+         * to complete
+         */
+
+        return branchGroup;
+    }
 
     public static void main(String[] args) {
-        Ouf3D staticScene = new Ouf3D();
+
+        BranchGroup branchGroup = observable();
+
+        branchGroup.compile();
+
+        new Laboratory(branchGroup);
     }
 
-
-    //In this method, the objects for the scene are generated and added to
-    //the SimpleUniverse.
-
-    public void createSceneGraph(SimpleUniverse su) {
+    private void createSceneGraph(SimpleUniverse su, Node node) {
 
         BranchGroup group = new BranchGroup();
 
-        float h = 0.5f, r = 0.1f;
-
-        Transform3D transform3D = new Transform3D();
-
-        transform3D.setTranslation(new Vector3d(0.0, h / 2.0, 0.0));
-
-        TransformGroup transformGroup = new TransformGroup(transform3D);
-
-        Cylinder cylinder = new Cylinder(r, h);
-
-        transformGroup.addChild(cylinder);
-
-        group.addChild(transformGroup);
+        group.addChild(node);
 
         group.compile();
 
         su.addBranchGraph(group);
     }
 
-    public void addAxis(SimpleUniverse su) {
+    private void addAxis(SimpleUniverse su) {
 
         BranchGroup group = new BranchGroup();
 
@@ -140,9 +122,7 @@ public class Ouf3D extends JFrame {
         su.addBranchGraph(group);
     }
 
-
-    //Some light is added to the scene here.
-    public void addLight(SimpleUniverse su) {
+    private void addLight(SimpleUniverse su) {
 
         BranchGroup bgLight = new BranchGroup();
 
