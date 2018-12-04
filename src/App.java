83b7eedaec9ffcafc6cc20e7fe6ecad4b21a5c5e
laroyenneg <laroyenneg@gmail.com>
2018-12-04 17:29:29 +0100

    demo

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import model.element.connexion.constant.axis.move.MoveY;
 import model.element.connexion.constant.axis.move.MoveZ;
 import model.element.connexion.constant.axis.rotation.Rolling;
 import model.element.connexion.joint.rotation.Revolving;
+import model.element.connexion.joint.rotation.Rotational;
 import model.element.connexion.joint.rotation.Twisting;
 import model.element.robot.Robot;
 import view.RobotView;
@@ -22,7 +23,7 @@ public class App {
 
     private static class TestRobot extends Robot {
         public TestRobot() {
-            super(new MoveY(0.1), new Twisting(0.0), new MoveZ(0.2), new MoveY(0.1), new Revolving(Math.PI / 3.0), new Rolling(-Math.PI / 2.0), new MoveY(0.6));
+            super(new MoveY(0.1), new Twisting(0.0), new MoveZ(0.2), new Rotational(Math.PI / 2.0), new MoveY(0.1), new Revolving(Math.PI / 3.0), new Rolling(-Math.PI / 2.0), new MoveY(0.6));
         }
     }
 }
