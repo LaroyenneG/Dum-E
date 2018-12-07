c28732dab8dc053405a9f01cad85176a72ef7119
laroyenneg <laroyenneg@gmail.com>
2018-12-07 22:08:08 +0100

    clean up

---
+++ src/model/element/Solver.java
@@ -0,0 +1,19 @@
+package model.element;
+
+import javax.vecmath.Point3d;
+
+public class Solver {
+
+    private NumerousJoints numerousJoints;
+
+    private double step;
+
+    public Solver(NumerousJoints numerousJoints) {
+        this.numerousJoints = numerousJoints;
+    }
+
+    public double[] compute(Point3d point3d) {
+
+        return null;
+    }
+}
